b9181f0645a205bfaa39b85879a74070503d4cdb
James Richardson <icyfiremario797@gmail.com>
2024-09-19 11:34:16 -0600

    Final toches

--- src/App.js
+++ src/App.js
@@ -22,29 +22,31 @@ function Checklist() {
     event.preventDefault();
 
     let assets = event.target.assets.checked;
+    let assets_2 = event.target.assets_2.checked
     let protect = event.target.protect.checked;
     let detect = event.target.detect.checked;
     let respond = event.target.respond.checked;
     let recover = event.target.recover.checked;
 
-    if (assets && protect && detect && respond && recover)
+    if (assets && assets_2 && protect && detect && respond && recover)
     {
-      window.alert("Wow your secured")
+      window.alert("Wow your secured, very mindful, very demure")
     }
     else
     {
-      window.alert("YOUR NOT SECURE")
+      window.alert("Please make sure you checked everything on the list")
     }
   }
 
   return (
     <div className="Checklist" onSubmit={check}>
       <form className="Checklist-form">
-        <Item id="assets">Identify potential security risks and points of failure</Item>
-        <Item id="protect">Identify how you can midgate and prevent you data being stolen</Item>
-        <Item id="detect">Setup ways to detect if your networks security has been breached</Item>
-        <Item id="respond">Setup protocalls for responding to security breaches</Item>
-        <Item id="recover">Recover your security and return the network to normal function</Item>
+        <Item id="assets">Identify all the devices connected to your network.</Item>
+        <Item id="assets_2">Assess each device for potental security risks such as open ports, using an unsecure wifi authentication protocall, and potentally unsecure processes.</Item>
+        <Item id="protect">Close unused ports if they are open, create a seperate network for devices using an unsecure wifi protocall, and shutdown any unneeded processes.</Item>
+        <Item id="detect">Install antivirus on all devices, and enable logging on your network.</Item>
+        <Item id="respond">Create a step by step checklist detailing how to recover your network if a security breach does occur.</Item>
+        <Item id="recover">Execute your recovery response protocall when a security breach does occur.</Item>
         <input type="submit" className="Checklist-submit" value="Done"/>
       </form>
     </div>
